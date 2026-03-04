8a3467a89b6cbae3218f5addc81fbad63daa1026
Clodoaldo Maldonado <clodoaldo.maldonado1972@gmail.com>
2026-03-04 20:20:43 -0300

    Adicionando lógica de sequência de leitura e integração com banco de dados

--- teste.py
+++ teste.py
@@ -2,7 +2,8 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.Text("Se você está lendo isso, o Flet está funcionando!"))
+    page.add(ft.Text("O Flet está funcionando!", size=30))
+    page.add(ft.ElevatedButton("Clique aqui"))
 
 
 ft.app(target=main)
