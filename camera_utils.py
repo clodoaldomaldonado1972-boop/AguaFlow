1124df9d7198e195afcbcd02660ff7dd6282a759
Clodoaldo Maldonado <clodoaldo.maldonado1972@gmail.com>
2026-03-11 18:01:16 -0300

    Arch: Migrado main.py para async/await para suporte total a camera

--- camera_utils.py
+++ camera_utils.py
@@ -1,25 +1,24 @@
 import flet as ft
 import processamento
 
+# Adicionamos 'async' aqui
 
-def inicializar_camera(page, ao_concluir_ocr):
-    # Limpeza profunda para evitar o erro de "Unknown Control" no Samsung M14
+
+async def inicializar_camera(page, ao_concluir_ocr):
     for control in page.overlay[:]:
         if isinstance(control, ft.FilePicker):
             page.overlay.remove(control)
 
-    def resultado_selecao(e: ft.FilePickerResultEvent):
+    async def resultado_selecao(e: ft.FilePickerResultEvent):
         if e.files:
             caminho = e.files[0].path
-            # Chama o módulo de visão computacional
             id_qr, valor_ocr = processamento.processar_foto_hidrometro(caminho)
             ao_concluir_ocr(id_qr, valor_ocr)
         page.update()
 
-    # Criando o seletor de forma resiliente
     seletor = ft.FilePicker()
     seletor.on_result = resultado_selecao
 
     page.overlay.append(seletor)
-    page.update()  # IMPORTANTE: Sincroniza o componente com o celular
+    page.update()
     return seletor
