8afc260b3a62fac2b68c60c60a4e08240e2d8e9d
Clodoaldo Maldonado <clodoaldo.maldonado1972@gmail.com>
2026-03-11 18:41:50 -0300

    Fix: Removido erro de sintaxe no camera_utils

--- camera_utils.py
+++ camera_utils.py
@@ -3,32 +3,26 @@ import processamento
 
 
 async def inicializar_camera(page: ft.Page, ao_concluir_ocr):
-    # 1. Limpeza de controles antigos (Fundamental para o Android)
+    # Remova aquele 'a' que estava aqui em cima!
+
+    # Limpeza de controles antigos
     for control in page.overlay[:]:
         if isinstance(control, ft.FilePicker):
             page.overlay.remove(control)
 
-    # 2. Resposta à seleção da foto
-    a  # No camera_utils.py:
-
-    # Dentro do camera_utils.py
-
     async def resultado_selecao(e: ft.FilePickerResultEvent):
         if e.files:
             caminho = e.files[0].path
-            # Processamento OCR
             id_qr, valor_ocr = processamento.processar_foto_hidrometro(caminho)
             ao_concluir_ocr(id_qr, valor_ocr)
 
-        # CORREÇÃO AQUI: Mude de await page.update_async() para:
+        # Lembre-se: apenas page.update()
         page.update()
 
     seletor = ft.FilePicker()
     seletor.on_result = resultado_selecao
 
     page.overlay.append(seletor)
-
-    # CORREÇÃO AQUI: Mude de await page.update_async() para:
     page.update()
 
     return seletor
